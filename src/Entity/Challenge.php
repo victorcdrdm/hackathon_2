72da4da9f0fee7ad4f02368dcfe072a2829da37e
Claire812 <clairedaudignon@gmail.com>
2021-01-13 14:08:38 +0100

    creation entities

---
+++ src/Entity/Challenge.php
@@ -0,0 +1,94 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ChallengeRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=ChallengeRepository::class)
+ */
+class Challenge
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\ManyToOne(targetEntity=User::class, inversedBy="challenges")
+     * @ORM\JoinColumn(nullable=false)
+     */
+    private $creator;
+
+    /**
+     * @ORM\ManyToOne(targetEntity=User::class, inversedBy="catcherChallenges")
+     */
+    private $catcher;
+
+    /**
+     * @ORM\ManyToOne(targetEntity=Defi::class)
+     * @ORM\JoinColumn(nullable=false)
+     */
+    private $defi;
+
+    /**
+     * @ORM\Column(type="boolean")
+     */
+    private $isSuccess;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getCreator(): ?User
+    {
+        return $this->creator;
+    }
+
+    public function setCreator(?User $creator): self
+    {
+        $this->creator = $creator;
+
+        return $this;
+    }
+
+    public function getCatcher(): ?User
+    {
+        return $this->catcher;
+    }
+
+    public function setCatcher(?User $catcher): self
+    {
+        $this->catcher = $catcher;
+
+        return $this;
+    }
+
+    public function getDefi(): ?Defi
+    {
+        return $this->defi;
+    }
+
+    public function setDefi(?Defi $defi): self
+    {
+        $this->defi = $defi;
+
+        return $this;
+    }
+
+    public function getIsSuccess(): ?bool
+    {
+        return $this->isSuccess;
+    }
+
+    public function setIsSuccess(bool $isSuccess): self
+    {
+        $this->isSuccess = $isSuccess;
+
+        return $this;
+    }
+}
