5815bf8f7b2878f0e0669201fc13645b20b4be11
victorcdrdm <victorcrombez@gmail.com>
2021-01-14 18:15:55 +0100

    validate done

--- src/Form/ValidateType.php
+++ src/Form/ValidateType.php
@@ -3,7 +3,10 @@
 namespace App\Form;
 
 use App\Entity\Challenge;
+use phpDocumentor\Reflection\Types\Boolean;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -12,7 +15,10 @@ class ValidateType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('isValid')
+            ->add('isValid', CheckboxType::class,
+            [
+                'required' => false,
+            ])
 
         ;
     }
