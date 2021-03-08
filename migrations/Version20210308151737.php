088864137f97f44aec99cfcb9fa4714dc4125910
FromBenj <touboul.benjamin@gmail.com>
2021-03-08 16:26:20 +0100

    C logo links

--- migrations/Version20210308151737.php
+++ migrations/Version20210308151737.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20210114194907 extends AbstractMigration
+final class Version20210308151737 extends AbstractMigration
 {
     public function getDescription() : string
     {
