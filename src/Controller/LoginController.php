b877cc7e3de4ba780d006b76ad595d06b16f8a7e
FromBenj <touboul.benjamin@gmail.com>
2021-03-08 17:28:46 +0100

    C links updated

--- src/Controller/LoginController.php
+++ src/Controller/LoginController.php
@@ -24,7 +24,10 @@ class LoginController extends AbstractController
         $lastUsername = $authenticationUtils->getLastUsername();
 
 
-        return $this->render('security/login.html.twig', ['last_username' => $lastUsername, 'error' => $error]);
+        return $this->render('security/login.html.twig', ['last_username' => $lastUsername,
+            'error'   => $error,
+            'reg_log' => true,
+        ]);
 
     }
 
